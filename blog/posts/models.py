0a952211dacd4ab1e67521780f5df618a89b5834
neveralways <3844759+neveralways@users.noreply.github.com>
2024-12-07 18:24:43 +0100

    First model creation

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
