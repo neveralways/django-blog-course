9c288ff034399014e64dc9d733d4a5b358e4b6e6
Neveralways <3844759+neveralways@users.noreply.github.com>
2024-12-08 18:52:38 +0100

    Migrations

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=60)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateField(auto_now_add=True)
