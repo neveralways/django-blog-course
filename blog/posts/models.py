129b6872fc8dc8ef2b4582939b84f68baaf2c569
neveralways <3844759+neveralways@users.noreply.github.com>
2024-12-23 12:08:32 +0100

    Register model in admin panel

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=60)
     content = models.TextField()
     published_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
