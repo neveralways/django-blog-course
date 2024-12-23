58dd9473db04adb9d5378e63fcc081b582301501
neveralways <3844759+neveralways@users.noreply.github.com>
2024-12-23 12:15:19 +0100

    List_display and list_display links. Another way to register in admin

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'published_date')
+    list_display_links = ('id', 'title')
